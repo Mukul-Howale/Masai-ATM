4ee1cb1198ecb192eb72530547be28d95f42c949
Mukul Howale <howalemukul@gmail.com>
2023-03-02 18:16:31 +0530

    Update User.java

--- src/main/java/com/example/masaiatm/entity/User.java
+++ src/main/java/com/example/masaiatm/entity/User.java
@@ -19,7 +19,6 @@ public class User {
     private String firstName;
     private String lastName;
     private String address;
-    
     private String gender;
     private int mobileNumber;
     private int age;
